0c3a89ca7e5f8d5b22774e882d11a4c88e668a73
bw007 <abdulaziz00791@gmail.com>
2024-03-12 07:59:18 +0500

    Update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const barIcon = document.querySelector(".header__menu-bar"),
+  sidebar = document.querySelector(".header__sidebar");
+
+  barIcon.onclick = (e) => {
+    barIcon.classList.toggle("header__menu-bar--active")
+    sidebar.classList.toggle("header__sidebar--active");
+  }
+
+  window.addEventListener("resize", () => {
+    barIcon.classList.remove("header__menu-bar--active")
+    sidebar.classList.remove("header__sidebar--active");
+  })
