760dbb9f2443d93d450d5d61f5c5e1705977a626
bw007 <mr.abdulaziz00791@gmail.com>
2024-03-12 17:01:47 +0500

    Updated

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 const barIcon = document.querySelector(".header__menu-bar"),
   sidebar = document.querySelector(".header__sidebar");
 
-  barIcon.onclick = (e) => {
-    barIcon.classList.toggle("header__menu-bar--active")
+  barIcon.onclick = () => {
+    barIcon.classList.toggle("header__menu-bar--active");
     sidebar.classList.toggle("header__sidebar--active");
-    document.body.classList.toggle("overflow")
-  }
+    document.body.classList.toggle("overflow");
+  };
 
   window.addEventListener("resize", () => {
-    barIcon.classList.remove("header__menu-bar--active")
+    barIcon.classList.remove("header__menu-bar--active");
     sidebar.classList.remove("header__sidebar--active");
-  })
+  });
+
+const links = document.querySelectorAll(".header__sidebar .header__link");
+
+links.forEach(el => {
+  el.onclick = () => {
+    barIcon.classList.remove("header__menu-bar--active");
+    sidebar.classList.remove("header__sidebar--active");
+  };
+});
